33eab8d508cf06f7985eac4f42e7f78c310eac99
leafeon-b <grad.f.ishida@gmail.com>
2022-07-24 13:15:26 +0000

    サーバ側のコードを追加

---
+++ server/src/socket.ts
@@ -0,0 +1,14 @@
+import { Server, Socket } from "socket.io";
+
+function socket({io}: {io: Server}){
+
+    io.on("connection", (socket: Socket) => {
+        console.log(`User connected ${socket.id}`);
+
+        socket.on("sendMessage", (message) => {
+            socket.emit("responseMessage", message);
+        });
+    });
+}
+
+export default socket;

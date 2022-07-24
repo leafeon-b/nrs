e56731418c3522366b91085b6287d0fbdba14e5c
leafeon-b <grad.f.ishida@gmail.com>
2022-07-24 13:06:23 +0000

    React側でsocketを使う準備

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useSockets} from './context/socket.context';
 
 function App() {
+  const {socket, messages, setMessages} = useSockets();
+  const messageRef = useRef(null);
+  console.log(messages);
+
+  function handleClick() {
+    const message = messageRef.current.value;
+    if (!String(message).trim()) return;
+
+    socket.emit("sendMessage", message);
+
+    messageRef.current.value = "";
+  }
+
+  socket.on("responseMessage", (message) => {
+    console.log(message);
+    setMessages([...messages, message]);
+    console.log(messages);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" ref={messageRef} placeholder="write message" />
+      <button onClick={handleClick}>Send</button>
+      <Messages />
+        
+    </>
+  );
+}
+
+function Messages() {
+  const {socket, messages, setMessages} = useSockets();
+  return (<>
+    {messages && (<div>
+      {messages.map(({message}, index) => {
+        return <li key={index}>{message}</li>
+      })}
+    </div>)}
+  </>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SocketsProvider from './context/socket.context'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <SocketsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </SocketsProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
